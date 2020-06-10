f3ec8d2b1ea9490c3eed635a397e830048a80a22
Sergey Matyukevich <geomatsi@gmail.com>
2020-06-10 23:48:00 +0300

    ofono: use dbus to get list of modems
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- dbus-ofono/go.mod
+++ dbus-ofono/go.mod
@@ -1,3 +1,5 @@
 module go-ofono
 
 go 1.13
+
+require github.com/godbus/dbus/v5 v5.0.3
--- dbus-ofono/main.go
+++ dbus-ofono/main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
+	"reflect"
+
+	dbus "github.com/godbus/dbus/v5"
 )
 
 func main() {
-	log.Printf("Hello World")
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to connect to session bus:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	var res [][]interface{}
+
+	err = conn.Object("org.ofono", dbus.ObjectPath("/")).Call("org.ofono.Manager.GetModems", 0).Store(&res)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to get list of owned names:", err)
+		os.Exit(1)
+	}
+
+	for _, t := range res {
+		switch reflect.TypeOf(t).Kind() {
+		case reflect.Slice:
+			s := reflect.ValueOf(t)
+
+			for i := 0; i < s.Len(); i++ {
+				v := s.Index(i)
+				fmt.Printf("type[%v] kind[%v] --> %v\n",
+					reflect.TypeOf(v), reflect.TypeOf(v).Kind(), v)
+			}
+		}
+	}
 }
