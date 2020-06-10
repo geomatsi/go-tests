f322e76809022c7f171b6ed78ba74a716b1aa91b
Sergey Matyukevich <geomatsi@gmail.com>
2020-06-10 23:54:15 +0300

    ofono: extract common dbus commands
    Extract common DBus patterns into utility functions.
    Implement network context enable/disable operations.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- dbus-ofono/main.go
+++ dbus-ofono/main.go
@@ -34,6 +34,13 @@ func main() {
 			os.Exit(1)
 		}
 		dbusModem(conn, args[2], args[3])
+	case "context":
+		if len(args) < 4 {
+			help(args)
+			os.Exit(1)
+		}
+		dbusContext(conn, args[2], args[3])
+
 	case "intro":
 		if len(args) < 4 {
 			help(args)
@@ -60,58 +67,124 @@ func dbusIntro(conn *dbus.Conn, service string, path string) {
 }
 
 func dbusListModems(conn *dbus.Conn) {
-	var modems [][]interface{}
+	utilSignature1(conn, "/", "org.ofono.Manager.GetModems", false)
+}
 
-	// org.ofono.Manager.GetModems: a(oa{sv})
-	err := conn.Object("org.ofono", dbus.ObjectPath("/")).Call("org.ofono.Manager.GetModems", 0).Store(&modems)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Failed to get available modems:", err)
+func dbusContext(conn *dbus.Conn, cmd string, context string) {
+	var method string = "org.ofono.ConnectionContext.SetProperty"
+	var active dbus.Variant
+
+	switch cmd {
+	case "enable":
+		active = dbus.MakeVariant(true)
+	case "disable":
+		active = dbus.MakeVariant(false)
+	default:
+		fmt.Printf("Unknown context command: %s\n", cmd)
 		return
 	}
 
-	for _, mx := range modems {
-		switch reflect.TypeOf(mx).Kind() {
-		case reflect.Slice:
-			for _, m := range mx {
-				switch m.(type) {
-				case dbus.ObjectPath:
-					fmt.Printf("modem %s\n", m)
-				}
-			}
-		}
+	err := conn.Object("org.ofono", dbus.ObjectPath(context)).Call(method, 0, "Active", active).Store()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to call DBus method:", err)
+		return
 	}
 }
 
 func dbusModem(conn *dbus.Conn, cmd string, modem string) {
-	// DBus type: a{sv}
-	var res map[string]dbus.Variant
+	var signature string
 	var method string
 
 	switch cmd {
 	case "dump":
 		method = "org.ofono.Modem.GetProperties"
-		fmt.Printf("Modem %s properties:\n", modem)
+		signature = "a{sv}"
 	case "sim":
 		method = "org.ofono.SimManager.GetProperties"
-		fmt.Printf("Modem %s SIM properties:\n", modem)
+		signature = "a{sv}"
 	case "stats":
 		method = "org.ofono.cinterion.HardwareMonitor.GetStatistics"
-		fmt.Printf("Modem %s hardware stats:\n", modem)
+		signature = "a{sv}"
 	case "aps":
 		method = "org.ofono.AllowedAccessPoints.GetAllowedAccessPoints"
-		fmt.Printf("Modem %s APs:\n", modem)
+		signature = "as"
 	case "conn":
 		method = "org.ofono.ConnectionManager.GetProperties"
-		fmt.Printf("Modem %s connection properties:\n", modem)
+		signature = "a{sv}"
 	case "reg":
 		method = "org.ofono.NetworkRegistration.GetProperties"
-		fmt.Printf("Modem %s connection properties:\n", modem)
+		signature = "a{sv}"
+	case "ctx":
+		method = "org.ofono.ConnectionManager.GetContexts"
+		signature = "a(oa{sv})"
 	default:
 		fmt.Printf("Unknown modem command: %s\n", cmd)
 		return
 	}
 
-	err := conn.Object("org.ofono", dbus.ObjectPath(modem)).Call(method, 0).Store(&res)
+	fmt.Printf("modem[%s] method[%s] signature[%s]:\n",
+		modem, method, signature)
+
+	switch signature {
+	case "a(oa{sv})":
+		utilSignature1(conn, modem, method, true)
+	case "a{sv}":
+		utilSignature2(conn, modem, method)
+	case "as":
+		utilSignature3(conn, modem, method)
+	}
+
+	return
+}
+
+// signature: a(oa{sv})
+func utilSignature1(conn *dbus.Conn, path string, method string, dump bool) {
+	var res [][]interface{}
+
+	err := conn.Object("org.ofono", dbus.ObjectPath(path)).Call(method, 0).Store(&res)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to call DBus method:", err)
+		return
+	}
+
+	fmt.Println("{")
+
+	for _, e := range res {
+		switch reflect.TypeOf(e).Kind() {
+		case reflect.Slice:
+			for _, m := range e {
+				switch val := m.(type) {
+				case dbus.ObjectPath:
+					fmt.Printf("\t%s\n", val)
+				case map[string]dbus.Variant:
+					if dump {
+						fmt.Println("\t{")
+
+						for k, v := range val {
+							fmt.Printf("\t\t%s: %v\n", k, v)
+						}
+
+						fmt.Println("\t}")
+					}
+				default:
+					fmt.Printf("\t%s -> %s\n",
+						reflect.TypeOf(m), reflect.TypeOf(m).Kind())
+				}
+			}
+		default:
+			fmt.Printf("\t{\n\t\t%s -> %s\n\t}\n",
+				reflect.TypeOf(e), reflect.TypeOf(e).Kind())
+		}
+	}
+
+	fmt.Println("}")
+}
+
+// signature: a{sv}
+func utilSignature2(conn *dbus.Conn, path string, method string) {
+	var res map[string]dbus.Variant
+
+	err := conn.Object("org.ofono", dbus.ObjectPath(path)).Call(method, 0).Store(&res)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to call DBus method:", err)
 		return
@@ -124,7 +197,25 @@ func dbusModem(conn *dbus.Conn, cmd string, modem string) {
 	}
 
 	fmt.Println("}")
-	return
+}
+
+// signature: as
+func utilSignature3(conn *dbus.Conn, path string, method string) {
+	var res []string
+
+	err := conn.Object("org.ofono", dbus.ObjectPath(path)).Call(method, 0).Store(&res)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to call DBus method:", err)
+		return
+	}
+
+	fmt.Println("{")
+
+	for _, v := range res {
+		fmt.Printf("\t%s\n", v)
+	}
+
+	fmt.Println("}")
 }
 
 func help(args []string) {
@@ -137,4 +228,7 @@ func help(args []string) {
 	fmt.Printf("\t%s modem aps <name> - list modem Access Points\n", args[0])
 	fmt.Printf("\t%s modem conn <name> - list modem connection properties\n", args[0])
 	fmt.Printf("\t%s modem reg <name> - list modem network registration properties\n", args[0])
+	fmt.Printf("\t%s modem ctx <name> - list modem network contexts\n", args[0])
+	fmt.Printf("\t%s context enable <name> - enable network context\n", args[0])
+	fmt.Printf("\t%s context disable <name> - disable network context\n", args[0])
 }
