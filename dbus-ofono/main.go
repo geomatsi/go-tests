a1ac3c751604d7c8ec487efdcf5dfb8c1fe7ecd4
Sergey Matyukevich <geomatsi@gmail.com>
2020-06-10 23:49:36 +0300

    ofono: get modem info
    Implement dbus calls retrieving modem information:
    - list of modems
    - modem properties
    - cinterion specific modem stats
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- dbus-ofono/main.go
+++ dbus-ofono/main.go
@@ -1,38 +1,116 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"reflect"
 
 	dbus "github.com/godbus/dbus/v5"
+	introspect "github.com/godbus/dbus/v5/introspect"
 )
 
 func main() {
 	conn, err := dbus.SystemBus()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Failed to connect to session bus:", err)
+		fmt.Fprintln(os.Stderr, "Failed to connect to system bus:", err)
 		os.Exit(1)
 	}
 	defer conn.Close()
 
-	var res [][]interface{}
+	// dbus service introspection
 
-	err = conn.Object("org.ofono", dbus.ObjectPath("/")).Call("org.ofono.Manager.GetModems", 0).Store(&res)
+	args := os.Args
+
+	fmt.Printf("Introspection: %v -> %v\n", args[1], args[2])
+	node, err := introspect.Call(conn.Object(args[1], dbus.ObjectPath(args[2])))
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Failed to get list of owned names:", err)
+		fmt.Fprintln(os.Stderr, "Failed to perform introspection:", err)
 		os.Exit(1)
 	}
 
-	for _, t := range res {
-		switch reflect.TypeOf(t).Kind() {
-		case reflect.Slice:
-			s := reflect.ValueOf(t)
+	data, _ := json.MarshalIndent(node, "", "    ")
+	os.Stdout.Write(data)
+	fmt.Println("")
 
-			for i := 0; i < s.Len(); i++ {
-				v := s.Index(i)
-				fmt.Printf("type[%v] kind[%v] --> %v\n",
-					reflect.TypeOf(v), reflect.TypeOf(v).Kind(), v)
+	// org.ofono.Manager.GetModems: a(oa{sv})
+
+	var modems [][]interface{}
+
+	err = conn.Object("org.ofono", dbus.ObjectPath("/")).Call("org.ofono.Manager.GetModems", 0).Store(&modems)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to get the list of available modems:", err)
+		os.Exit(1)
+	}
+
+	for _, mx := range modems {
+		switch reflect.TypeOf(mx).Kind() {
+		case reflect.Slice:
+			for _, m := range mx {
+				switch m.(type) {
+				case dbus.ObjectPath:
+
+					// org.ofono.Modem.GetProperties: a{sv}
+
+					var mp map[string]dbus.Variant
+
+					fmt.Printf("Modem %s properties:\n", m)
+
+					err = conn.Object("org.ofono", m.(dbus.ObjectPath)).Call("org.ofono.Modem.GetProperties", 0).Store(&mp)
+					if err != nil {
+						fmt.Fprintln(os.Stderr, "Failed to get the list of properties:", err)
+						os.Exit(1)
+					}
+
+					fmt.Println("{")
+
+					for k, v := range mp {
+						fmt.Printf("\t%s -> %v\n", k, v)
+					}
+
+					fmt.Println("}")
+
+					// org.ofono.cinterion.HardwareMonitor.GetStatistics: a{sv}
+
+					var ms map[string]dbus.Variant
+
+					fmt.Printf("Modem %s stats:\n", m)
+
+					err = conn.Object("org.ofono", m.(dbus.ObjectPath)).Call("org.ofono.cinterion.HardwareMonitor.GetStatistics", 0).Store(&ms)
+					if err != nil {
+						fmt.Fprintln(os.Stderr, "Failed to get the cinterion stats:", err)
+						os.Exit(1)
+					}
+
+					fmt.Println("{")
+
+					for k, v := range ms {
+						fmt.Printf("\t%s -> %v\n", k, v)
+					}
+
+					fmt.Println("}")
+
+					// org.ofono.SimManager.GetProperties: a{sv}
+
+					var sp map[string]dbus.Variant
+
+					fmt.Printf("Modem %s SIM properties:\n", m)
+
+					err = conn.Object("org.ofono", m.(dbus.ObjectPath)).Call("org.ofono.SimManager.GetProperties", 0).Store(&sp)
+					if err != nil {
+						fmt.Fprintln(os.Stderr, "Failed to get the cinterion stats:", err)
+						os.Exit(1)
+					}
+
+					fmt.Println("{")
+
+					for k, v := range sp {
+						fmt.Printf("\t%s -> %v\n", k, v)
+					}
+
+					fmt.Println("}")
+
+				}
 			}
 		}
 	}
